c4bda8e455288c82737894e0bbc2b4a20e0f89a0
Oleksandr Tsyhanok <shrek1337@icloud.com>
2026-01-28 21:59:55 +0200

    updated telemetry model && event model with acknowledged field

--- backend/apps/rules/models/event.py
+++ backend/apps/rules/models/event.py
@@ -5,6 +5,7 @@ class Event(models.Model):
     id = models.BigAutoField(primary_key=True)
     timestamp = models.DateTimeField(default=models.functions.Now(), null=False, db_index=True)
     rule = models.ForeignKey('rules.Rule', on_delete=models.CASCADE, null=False, db_index=True)
+    acknowledged = models.BooleanField(default=False, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True, null=False)
 
     class Meta:
