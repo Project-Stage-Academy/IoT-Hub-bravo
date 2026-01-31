032845a215e8e7304664c08c874678818ca627f9
ssannkkiiii <oleksandrhredil71@gmail.com>
2026-01-31 14:06:44 +0200

    add client ip resolver for checking real user ip address

--- backend/conf/middleware/rate_limit.py
+++ backend/conf/middleware/rate_limit.py
@@ -2,6 +2,11 @@ from django.conf import settings
 import re
 import logging
 
+import ipaddress 
+from django.core.cache import cache
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+
 logger = logging.getLogger(__name__)
 
 """
@@ -23,3 +28,52 @@ def _compile_rules():
     PREFIX_RULES.sort(key=lambda x: -len(x[0]))
 
 _compile_rules()
+
+
+"""
+ClientIpResolver is a class that resolves the client IP address from the request.
+"""
+
+class ClientIpResolver:
+
+    @staticmethod
+    def get_ip(request):
+        remote_addr = request.META.get('REMOTE_ADDR')
+
+        if not remote_addr:
+            return '127.0.0.1'
+
+        if not ClientIPResolver._is_trusted_proxy(remote_addr):
+            return remote_addr
+
+        forwarded = request.META.get('HTTP_X_FORWARDED_FOR')
+
+        if not forwarded:
+            return remote_addr
+
+        ips = [ip.strip() for ip in forwarded.split(',')]
+
+        for ip in reversed(ips):
+            try:
+                ipaddress.ip_address(ip)
+                return ip
+            except ValueError:
+                continue
+
+        return remote_addr
+
+        @staticmethod
+        def _is_trusted_proxy(ip):
+            proxies = getattr(settings, "RATE_LIMIT_TRUSTED_PROXIES", [])
+
+        try:
+            remote_ip = ipaddress.ip_address(ip)
+
+            for net in proxies:
+                if remote_ip in ipaddress.ip_network(net):
+                    return True
+
+        except ValueError:
+            pass
+
+        return False
