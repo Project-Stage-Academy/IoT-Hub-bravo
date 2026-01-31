619da862283c3612463cfe5164aeac4ce64440fb
Andrei Tsuhui <fashiron228@gmail.com>
2026-01-31 11:22:24 +0200

    Black format

--- backend/conf/celery_app.py
+++ backend/conf/celery_app.py
@@ -1,8 +1,8 @@
 from celery import Celery
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 
-app = Celery('iot-hub')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("iot-hub")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -10,12 +10,12 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv(
 
 # Django apps
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 # Third party apps
@@ -61,7 +61,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "conf.wsgi.application"
 
 # Databases
-IS_BUILD = os.environ.get('BUILD_TIME') == '1'
+IS_BUILD = os.environ.get("BUILD_TIME") == "1"
 
 if IS_BUILD:
     DATABASES = {
@@ -107,19 +107,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = config(
-    'CORS_ALLOW_ALL_ORIGINS',
-    default=False,
-    cast=bool
-)
+CORS_ALLOW_ALL_ORIGINS = config("CORS_ALLOW_ALL_ORIGINS", default=False, cast=bool)
 
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS", default="", cast=lambda v: v.split(",") if v else []
@@ -171,12 +167,14 @@ CORS_ALLOW_HEADERS = [
 if USE_TZ:
     CELERY_TIMEZONE = TIME_ZONE
 
-CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://127.0.0.1:6379/0')
-CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://127.0.0.1:6379/0')
+CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="redis://127.0.0.1:6379/0")
+CELERY_RESULT_BACKEND = config(
+    "CELERY_RESULT_BACKEND", default="redis://127.0.0.1:6379/0"
+)
 
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 CELERY_TASK_ACKS_LATE = True
 CELERY_TASK_SEND_SENT_EVENT = True
