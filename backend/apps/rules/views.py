6b00edd0b9ba7c0efeb99d6ada77a9e1c0b880b1
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2026-02-02 02:37:12 +0200

    Update views.py

--- backend/apps/rules/views.py
+++ backend/apps/rules/views.py
@@ -1,20 +1,11 @@
 from django.http import HttpResponse
 import logging
 from django.http import HttpResponseServerError
-from prometheus_client import Counter
 
 logger = logging.getLogger("rules") # logger.setLevel(logging.INFO) - is default
-my_metric = Counter('my_metric_total', 'Description')
 
 def rules_index(request):
     """Home page for rules/"""
     logger.warning("Platform is running at risk", extra={"context": {"user_id": 12, "smt": "1123"}})
-    # raise PermissionDenied("You can't access this page")
-    # raise Http404("Page not found")
-    # value = "E41" 
-    # logger.info("some info")
-    # logger.critical("something really bad", extra={"custom_field": "some value", "error_code": value})
 
-    my_metric.inc()
-    return HttpResponseServerError("grafana test")    
-    # return HttpResponse("app is working 12312321")
+    return HttpResponseServerError("grafana test")
