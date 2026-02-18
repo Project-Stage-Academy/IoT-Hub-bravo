bdb18285cc6c8ad425abbdfeae4a18b35553a816
Leftmy <leftmyfrid0708@gmail.com>
2026-02-18 23:32:55 +0200

    Added "unit" field to Metric model, updated fixtures

--- backend/apps/devices/models/metric.py
+++ backend/apps/devices/models/metric.py
@@ -10,7 +10,10 @@ class MetricDataType(models.TextChoices):
 class Metric(models.Model):
     id = models.AutoField(primary_key=True)
     metric_type = models.CharField(
-        max_length=100, unique=True, null=False, db_collation='case_insensitive'
+        max_length=100, null=False, db_collation='case_insensitive', db_index=True
+    )
+    unit = models.CharField(
+        max_length=10, null=True,  db_collation='case_insensitive', db_index=True
     )
     data_type = models.CharField(
         max_length=10, choices=MetricDataType.choices, default=MetricDataType.NUMERIC
@@ -19,11 +22,15 @@ class Metric(models.Model):
     class Meta:
         db_table = 'metrics'
         constraints = [
+            models.UniqueConstraint(
+                fields=['metric_type', 'unit'],
+                name='unique_metric_type_with_unit',
+            ),
             models.CheckConstraint(
                 condition=models.Q(data_type__in=MetricDataType.values),
-                name="check_valid_device_metric_type",
-            )
+                name="check_valid_metric_data_type",
+            ),
         ]
 
     def __str__(self):
-        return self.metric_type
+        return f"{self.metric_type} ({self.unit})"
