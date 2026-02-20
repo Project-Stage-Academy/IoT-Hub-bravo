0c246529b02507914eb030aa194c546c1c7eee42
ssannkkiiii <oleksandrhredil71@gmail.com>
2026-02-20 11:24:45 +0200

    fix import

--- backend/conf/asgi.py
+++ backend/conf/asgi.py
@@ -1,14 +1,13 @@
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.devices.routing import websocket_urlpatterns
+from apps.users.middleware.channels_jwt import JWTAuthMiddleware
 from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from apps.devices.routing import websocket_urlpatterns
-from apps.users.middleware.channels_jwt import JWTAuthMiddleware
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+
 
 application = ProtocolTypeRouter(
     {
