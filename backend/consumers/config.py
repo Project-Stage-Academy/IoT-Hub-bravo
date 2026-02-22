b04428f2da3654a1f42b7693f16a9e87285ce3bf
Yaroslav Oryshchenko <y.o.oryshchenko@gmail.com>
2026-02-22 21:34:47 +0200

    fix env var data type cast

--- backend/consumers/config.py
+++ backend/consumers/config.py
@@ -9,7 +9,7 @@ class ConsumerConfig:
     group_id: str = config('KAFKA_GROUP_ID', default='kafka-consumer')
     auto_offset_reset: str = config('KAFKA_AUTO_OFFSET_RESET', default='earliest')
     enable_auto_commit: bool = config('KAFKA_ENABLE_AUTO_COMMIT', default=False, cast=bool)
-    auto_commit_interval_ms: int = config('KAFKA_AUTO_COMMIT_INTERVAL_MS', default=1000, cast=bool)
+    auto_commit_interval_ms: int = config('KAFKA_AUTO_COMMIT_INTERVAL_MS', default=1000, cast=int)
     session_timeout_ms: int = config('KAFKA_SESSION_TIMEOUT_MS', default=10000, cast=int)
     max_poll_interval_ms: int = config('KAFKA_MAX_POLL_INTERVAL_MS', default=300000, cast=int)
 
