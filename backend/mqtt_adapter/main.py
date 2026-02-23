9dd0c1d22eded83f6ba5a32c081fc0b0046bb842
Yaroslav Oryshchenko <y.o.oryshchenko@gmail.com>
2026-02-23 23:45:39 +0200

    add kafka producer handler to mqtt adapter

--- backend/mqtt_adapter/main.py
+++ backend/mqtt_adapter/main.py
@@ -1,22 +1,43 @@
-import os
+import logging
 
-import django
+from decouple import config
 
-from .config import MqttConfig
-from .mqtt_client import run_mqtt_client
-from .message_handlers import CeleryMessageHandler
+from producers.kafka_producer import KafkaProducer
+from producers.config import ProducerConfig
+from mqtt_adapter.config import MqttConfig
+from mqtt_adapter.mqtt_client import run_mqtt_client
+from mqtt_adapter.message_handlers import KafkaProducerMessageHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
-django.setup()
+topic = config('KAFKA_TOPIC_TELEMETRY_RAW', default='telemetry.raw')
 
-from apps.devices.tasks import ingest_telemetry_payload  # noqa
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        format='[%(asctime)s] %(levelname)s %(name)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+    logging.getLogger().setLevel(logging.INFO)
 
 
 def main() -> None:
+    setup_logging()
+
+    kafka_producer = KafkaProducer(
+        config=ProducerConfig(),
+        topic=topic,
+    )
+
+    message_handler = KafkaProducerMessageHandler(
+        producer=kafka_producer,
+        key_field='device',
+    )
+
     run_mqtt_client(
         config=MqttConfig(),
-        handler=CeleryMessageHandler(ingest_telemetry_payload),
+        handler=message_handler,
     )
+
+    kafka_producer.flush()
 
 
 if __name__ == '__main__':
