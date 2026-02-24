b56cda490a1a12cc1e03d4fe794bd35f8ea21745
Oleksandr Tsyhanok <shrek1337@icloud.com>
2026-02-24 16:10:49 +0200

    added multiprocess-aware Prometheus metrics endpoint

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -29,7 +30,7 @@ urlpatterns = [
     path("api/auth/login/", login, name="user-auth"),
     path("api/devices/", include("apps.devices.urls.device_urls")),
     path("api/telemetry/", include("apps.devices.urls.telemetry_urls")),
-    path('prometheus/', include('django_prometheus.urls')),  # access metrics at "prometheus/metrics/"
+    path('prometheus/', include('apps.common.urls')),
     path('rules/', include('apps.rules.urls')),
 ]
 
